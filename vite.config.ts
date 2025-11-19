2cced490264ed2c0bdfc59161bd9d12b340d02c1
Vikas Gupta <vksepm21@gmail.com>
2025-11-19 18:27:13 -0500

    updated github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/visual-proof-inscribed-semicircles/',
   server: {
     port: 3000,
     host: 'localhost',
